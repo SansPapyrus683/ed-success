afee96f8dd0fe9bd55e1c9435068b914441f2ddc
SansPapyrus683 <55369003+SansPapyrus683@users.noreply.github.com>
2022-07-22 13:44:57 -0700

    made stuff lowercase

--- clean/crime_clean.py
+++ clean/crime_clean.py
@@ -13,51 +13,52 @@ def strip_digits(s: str) -> str:
 
 fin = "data/crime/crime17raw.csv"
 fout = "data/crime/crime17.csv"
-want_vars = ["STATE", "COUNTY", "VIOLENT_CRIME", "PROPERTY_CRIME"]
+want_vars = ["state", "county", "violent_crime", "property_crime"]
 
 crime = pd.read_csv(fin)
-crime.columns = [c.upper().replace(" ", "_") for c in crime.columns]
+crime.columns = [c.lower().replace(" ", "_") for c in crime.columns]
 crime = crime[want_vars]
 
-crime.STATE.fillna(method="ffill", inplace=True)
+crime.state.fillna(method="ffill", inplace=True)
 crime.dropna(inplace=True)
 
-crime.STATE = crime.STATE.apply(lambda s: s.split('-')[0].strip().upper())
-crime.COUNTY = crime.COUNTY.apply(
-    lambda c: (c.upper()
-               .replace("COUNTY", "")
-               .replace("POLICE DEPARTMENT", "")
+crime.state = crime.state.apply(lambda s: s.split('-')[0].strip().lower())
+crime.county = crime.county.apply(
+    lambda c: (c.lower()
+               .replace("county", "")
+               .replace("police department", "")
                .strip())
 )
-crime.VIOLENT_CRIME = crime.VIOLENT_CRIME.apply(lambda n: int(n.replace(",", "")))
-crime.PROPERTY_CRIME = crime.PROPERTY_CRIME.apply(lambda n: int(n.replace(",", "")))
+crime.violent_crime = crime.violent_crime.apply(lambda n: int(n.replace(",", "")))
+crime.property_crime = crime.property_crime.apply(lambda n: int(n.replace(",", "")))
 
 bad = {
-    "AUGUSTA-RICHMOND",
-    "DE KALB",
-    "LA PORTE6",
-    "HARTSVILLE/TROUSDALE"
+    "augusta-richmond",
+    "de kalb",
+    "la porte6",
+    "hartsville/trousdale"
 }
-crime = crime.loc[~crime.COUNTY.isin(bad)]
+crime = crime.loc[~crime.county.isin(bad)]
 
-counties = pd.read_csv("data/areas/other17raw.csv")
+counties = pd.read_csv("data/areas/counties.csv")
 cty_fips = {}
 for _, c in counties.iterrows():
-    name = (c.NAME
-            .replace("MUNICIPIO", "")
-            .replace("COUNTY", "")
-            .replace("PARISH", "")
+    name = (c["name"]
+            .lower()
+            .replace("municipio", "")
+            .replace("county", "")
+            .replace("parish", "")
             .strip())
-    cty_fips[name] = c.FIPS_STATE, c.FIPS_COUNTY
+    cty_fips[name] = c.fips_state, c.fips_county
 
 exceptions = {
-    "WESTCHESTER PUBLIC SAFETY": "WESTCHESTER",
-    "SALT LAKE  UNIFIED": "SALT LAKE"
+    "westchester public safety": "westchester",
+    "salt lake  unified": "salt lake"
 }
-crime["FIPS_STATE"] = crime.COUNTY.apply(
+crime["fips_state"] = crime.county.apply(
     lambda c: cty_fips[exceptions.get(strip_digits(c), strip_digits(c))][0]
 )
-crime["FIPS_COUNTY"] = crime.COUNTY.apply(
+crime["fips_county"] = crime.county.apply(
     lambda c: cty_fips[exceptions.get(strip_digits(c), strip_digits(c))][1]
 )
 
